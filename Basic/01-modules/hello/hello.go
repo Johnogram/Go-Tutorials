8af8d42ab8bf2e31c3827cd8a08be6716e905abe
John <john.minns@missguided.com>
2022-03-26 18:00:25 +0000

    Adding multiple name functionality

--- Basic/01-modules/hello/hello.go
+++ Basic/01-modules/hello/hello.go
@@ -11,7 +11,8 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("John")
+	names := []string{"John", "Johno", "JohnOGram"}
+	message, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
